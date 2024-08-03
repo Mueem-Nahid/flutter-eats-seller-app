d0d167a434899c95ae6203562b745a16ebfa8bb4
Mueem-Nahid <mueem51@gmail.com>
2024-08-03 23:15:41 +0600

    # Feature - ui for sign in screen - add custom text field

--- lib/screens/authScreens/signin_screen.dart
+++ lib/screens/authScreens/signin_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_eats_seller_app/widgets/custom_text_field.dart';
 
 class SigninScreen extends StatefulWidget {
   const SigninScreen({super.key});
@@ -8,15 +9,64 @@ class SigninScreen extends StatefulWidget {
 }
 
 class _SigninScreenState extends State<SigninScreen> {
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
+    return SingleChildScrollView(
       child: Column(
         children: [
-          Text(
-            'Sign in',
-            style: TextStyle(fontSize: 30, color: Colors.grey),
-          )
+          Container(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Image.asset(
+                'assets/images/seller.png',
+                height: 270,
+              ),
+            ),
+          ),
+          Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  CustomTextField(
+                    textEditingController: emailTextEditingController,
+                    iconData: Icons.email,
+                    hintString: 'Email',
+                    isObscure: false,
+                    enabled: true,
+                  ),
+                  CustomTextField(
+                    textEditingController: passwordTextEditingController,
+                    iconData: Icons.password,
+                    hintString: 'Password',
+                    isObscure: true,
+                    enabled: true,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueAccent,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 50, vertical: 15),
+                    ),
+                    child: const Text(
+                      'Log in',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              ))
         ],
       ),
     );
---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,52 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatefulWidget {
+  TextEditingController? textEditingController;
+  IconData? iconData;
+  String? hintString;
+  bool? isObscure = true;
+  bool? enabled = true;
+
+  CustomTextField({
+    super.key,
+    this.textEditingController,
+    this.iconData,
+    this.enabled,
+    this.hintString,
+    this.isObscure,
+  });
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+      ),
+      padding: const EdgeInsets.all(8),
+      margin: const EdgeInsets.all(8),
+      child: TextFormField(
+        enabled: widget.enabled,
+        controller: widget.textEditingController,
+        obscureText: widget.isObscure!,
+        decoration: InputDecoration(
+            border: InputBorder.none,
+            prefixIcon: Icon(
+              widget.iconData,
+              color: Colors.blueAccent,
+            ),
+            contentPadding:
+                const EdgeInsets.symmetric(vertical: 12, horizontal: 0),
+            hintText: widget.hintString,
+            hintStyle: TextStyle(color: Colors.grey[400])),
+      ),
+    );
+  }
+}
