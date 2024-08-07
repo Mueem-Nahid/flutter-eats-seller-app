f6dc143bf68e898b43d84166012a68eeba3b3404
Mueem-Nahid <mueem51@gmail.com>
2024-08-07 21:57:26 +0600

    # Feature - add geocoding: ^2.0.1 , geolocator: ^7.7.0

--- lib/screens/authScreens/signup_screen.dart
+++ lib/screens/authScreens/signup_screen.dart
@@ -45,16 +45,16 @@ class _SignupScreenState extends State<SignupScreen> {
   // get my location method
   getCurrentLocation() async {
     // get user's current location in lat lon
-    // Position currentPosition =
-    //     await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    // //  convert into human readable address
-    // placemark = await placemarkFromCoordinates(
-    //     currentPosition.latitude, currentPosition.longitude);
-    // print('here location $placemark');
-    // Placemark firstPlacemark = placemark![0];
-    // fullAddress =
-    //     '${firstPlacemark.subThoroughfare} ${firstPlacemark.thoroughfare}, ${firstPlacemark.subLocality} ${firstPlacemark.locality}, ${firstPlacemark.subAdministrativeArea}, ${firstPlacemark.administrativeArea} ${firstPlacemark.postalCode}, ${firstPlacemark.country}';
-    // locationTextEditingController.text = fullAddress;
+    Position currentPosition =
+        await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    //  convert into human readable address
+    placemark = await placemarkFromCoordinates(
+        currentPosition.latitude, currentPosition.longitude);
+    print('here location $placemark');
+    Placemark firstPlacemark = placemark![0];
+    fullAddress =
+        '${firstPlacemark.subThoroughfare} ${firstPlacemark.thoroughfare}, ${firstPlacemark.subLocality} ${firstPlacemark.locality}, ${firstPlacemark.subAdministrativeArea}, ${firstPlacemark.administrativeArea} ${firstPlacemark.postalCode}, ${firstPlacemark.country}';
+    locationTextEditingController.text = fullAddress;
   }
 
   @override
